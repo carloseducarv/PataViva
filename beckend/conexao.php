493f0e416ccb0cd93b7d3fc47e644e43ca32f031
Carlos <carloseeduu2006@gmail.com>
2026-05-03 17:51:25 -0300

    Atualizando conexao.php

--- beckend/conexao.php
+++ beckend/conexao.php
@@ -1,7 +1,17 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "pataviva");
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "pataviva";
 
+// Criando a conexão
+$conn = new mysqli($host, $usuario, $senha, $banco);
+
+// Verificando se houve erro
 if ($conn->connect_error) {
-    die("Erro: " . $conn->connect_error);
+    die("Falha na conexão: " . $conn->connect_error);
 }
+
+// Ajustando para aceitar acentos (UTF-8)
+$conn->set_charset("utf8");
 ?>
